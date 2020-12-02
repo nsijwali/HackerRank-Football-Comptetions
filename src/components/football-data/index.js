7e19024ef30b6bc8cbf44ef61d5f1bde2efdd1f7
Nitin Sijwali <sijwali@Nitins-MacBook-Air.local>
2020-12-02 15:34:47 +0530

    final commit

--- src/components/football-data/index.js
+++ src/components/football-data/index.js
@@ -1,58 +1,86 @@
-
-import React, { Component } from "react";
-import "./index.css";
+import React, { Component } from 'react';
+import './index.css';
 const classNames = require('classnames');
 
 export default class FootballMatchesData extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedYear: null,
+			data: [],
+		};
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedYear: null
-    };
-  }
+	onClick = (year) => (e) => {
+		// Code written in next line is to take care of adding active class to selected year for css purpose.
+		this.setState({
+			selectedYear: year,
+		});
+		this.fetchData(year);
+	};
 
-  onClick = (year) => (e) => {
-    // Code written in next line is to take care of adding active class to selected year for css purpose.
-    this.setState({
-      selectedYear: year
-    })
-  }
+	fetchData = (year) => {
+		fetch(
+			`https://jsonmock.hackerrank.com/api/football_competitions?year=${year}`,
+			{
+				method: 'GET',
+				dataType: 'JSON',
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8',
+				},
+			},
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(result);
+				this.setState({
+					data: result.data,
+				});
+			});
+	};
+	render() {
+		var years = [2011, 2012, 2013, 2014, 2015, 2016, 2017];
+		return (
+			<div className='layout-row'>
+				<div className='section-title'>Select Year</div>
+				<ul className='sidebar' data-testid='year-list'>
+					{years.map((year, i) => {
+						return (
+							<li
+								className={classNames({
+									'sidebar-item': true,
+									active: this.state.selectedYear === year,
+								})}
+								onClick={this.onClick(year)}
+								key={year}
+							>
+								<a>{year}</a>
+							</li>
+						);
+					})}
+				</ul>
 
-  render() {
-    var years= [2011, 2012, 2013, 2014, 2015, 2016, 2017];
-    return (
-      <div className="layout-row">
-        <div className="section-title">Select Year</div>
-        <ul className="sidebar" data-testid="year-list">
-          {years.map((year, i) => {
-            return (
-              <li className={
-                classNames({
-                  'sidebar-item': true,
-                  'active': this.state.selectedYear === year
-                })
-              }
-              onClick={this.onClick(year)}
-              key={year}>
-                <a>{year}</a>
-              </li>
-            )
-          })}
-        </ul>
-
-        <section className="content">
-          <section>
-            <div className="total-matches" data-testid="total-matches"></div>
-            
-            <ul className="mr-20 matches styled" data-testid="match-list">
-              <li className="slide-up-fade-in"> </li>
-            </ul>
-          </section>
-
-          <div data-testid="no-result" className="slide-up-fade-in no-result"></div>
-        </section>
-      </div>
-    );
-  }
+				<section className='content'>
+					{this.state.data.length !== 0 ? (
+						<section>
+							<div className='total-matches' data-testid='total-matches'>
+								Total matches: {this.state.data.length}
+							</div>
+							<ul className='mr-20 matches styled' data-testid='match-list'>
+								{this.state.data.map(({ name, winner }, index) => (
+									<li className='slide-up-fade-in' key={index}>
+										Match {name} won by {winner}{' '}
+									</li>
+								))}
+							</ul>
+						</section>
+					) : (
+						<div data-testid='no-result' className='slide-up-fade-in no-result'>
+							No Matches Found
+						</div>
+					)}
+				</section>
+			</div>
+		);
+	}
 }
